84e471fd77fd87c2c5674c81c19ffa224967d94e
Raffi Utama <raffifebriandikau@gmail.com>
2021-11-10 21:44:21 +0700

    Add page transaksi masuk

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,6 +1,7 @@
 import CartList from '../pages/customer/CartList'
 import Checkout from '../pages/customer/Checkout'
 import Payment from '../pages/customer/Payment'
+import Order from '../pages/seller/Order'
 
 import { ROLES } from '../constants'
 
@@ -21,6 +22,12 @@ const cartRoutes = [
     path: '/payment',
     component: Payment,
     allowedRoles: [ROLES.CUSTOMER],
+    type: 'private'
+  },
+  {
+    path: '/order',
+    component: Order,
+    allowedRoles: [ROLES.SELLER],
     type: 'private'
   }
 //   TODO: route checkout sekalian di sini (?)
